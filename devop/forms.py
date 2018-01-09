70881c45070f437a7d9a037cff8929833046c317
cheng <cheng@dx.com>
2018-01-09 17:03:15 +0800

    Add Clean Cache Forms

--- devop/forms.py
+++ devop/forms.py
@@ -6,3 +6,8 @@ from django import forms
 
 class AccountForm(forms.Form):
     account = forms.CharField(label='域帐号', max_length=15)
+
+
+class FileForm(forms.Form):
+    title = forms.CharField(max_length=80)
+    filename = forms.FileField()
